4363d4ad49bd28f2c6fce61384bff2782eea9789
SandeepJagade <san.jagade@gmail.com>
2024-12-25 12:12:13 +0530

    commited from Asusi3

--- src/com/jagade/LoginValidation12.java
+++ src/com/jagade/LoginValidation12.java
@@ -6,7 +6,26 @@ public class LoginValidation12 {
 		String id = "adminxyz" ;
 				int password =123 ;
 				
+				if(id== "admin"){
+					System.out.println("User is Valid");
+					
+					if(password == 123) {
+					System.out.println("Password is Valid");
+					System.out.println("Login Successful :) ");
+				}
+				
+				else {
+					System.out.println("Password is Invalid");
+					System.out.println("Login Unsuccessful :( " );
+				}
+				
+	}
+	else {
+		
+		System.out.println("UserId is Invalid");
+		System.out.println("Login Unsuccsseful");
 		
 	}
 
+}
 }
